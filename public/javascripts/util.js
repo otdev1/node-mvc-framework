48b2eeceec454f889a47014e99f0f9c63083f831
otdev1 <66022538+otdev1@users.noreply.github.com>
2024-12-22 11:44:09 -0400

    Adding functionality to delete a post

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,7 +1,35 @@
-window.onload = () => 
+let baseurl = window.location.origin; 
+
+window.onload = () =>
 {
     if (window.location.href === "http://localhost:4001/posts")
-    {   
+    {
         document.getElementById("target").remove();
-    }        
+    } 
+}
+
+async function remove(element)
+{
+    const postId = element.value;
+
+    const url = baseurl+'/post/destroy/'+postId;
+
+    const options = {
+    method: 'DELETE'
+    };
+
+    try 
+    {
+        const response = await fetch(url, options);
+        if (!response.ok) 
+        {
+            throw new Error('Network response was not ok');
+        }
+
+        console.log(response);
+    } 
+    catch (error) 
+    {
+        console.error('There was a problem with the DELETE request:', error.message);
+    }
 }
