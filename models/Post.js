58e617db5f84891c87e578c9a7795969eb64d648
otdev1 <66022538+otdev1@users.noreply.github.com>
2025-01-07 19:58:09 -0400

    Updated Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,13 @@
 import * as Model from '../src/ModelBase.js';
 
 export default class Post extends Model.Base{
+
   setup() {
     this.setAttribute("title", Model.Attributes.String);
     this.setAttribute("content", Model.Attributes.String);
 
     this.validate("title", Model.Validators.PresenceOf);
-
+    
     this.beforeSave("prependTitle");
     this.afterSave("appendContent");
   }
@@ -14,12 +15,12 @@ export default class Post extends Model.Base{
   prependTitle() {
     if (this.title) 
     {
-      this.title = '-- ' + this.title;
+      this.title = this.title;
     }
   }
 
   appendContent() {
-    this.content = this.content + ' changed';
+    this.content = this.content;
   }
   
 }
